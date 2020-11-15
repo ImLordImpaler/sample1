00414c872ddb226329f55c278e0ee49d40beb142
ksfastners1 <ksfastners619@gmail.com>
2020-11-15 18:52:09 +0530

    Sample-1

--- bobbyLaal/settings.py
+++ bobbyLaal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'z$nn__v++pqt&_(u9=m03##253e+udupka90x$0%b47$v-$=wn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1' , 'salonsample.herokuapp.com']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
